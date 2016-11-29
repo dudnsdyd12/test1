a8850fd00bf5ea29b73f017c9ea575e05d770d37
YOY <dudnsdyd123@naver.com>
2016-11-29 18:24:16 +0900

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/소스.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class test
+{
+	int man;
+
+public:
+	void print() {
+		man = 1;
+		cout << "man = " << man << endl;
+	}
+};
+int main() {
+
+	return 0;
+}
